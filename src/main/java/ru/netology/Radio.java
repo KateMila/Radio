a431d6e47ce65c518dc0ee82553b0ecad01a532d
KateMila <89808903+KateMila@users.noreply.github.com>
2022-01-12 12:41:18 +0300

    Added tests

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,61 +3,67 @@ package ru.netology;
 public class Radio {
 
     private int currentStation;
-    private int currentSoundLevel;
+    private int currentVolume;
 
     public int getCurrentStation() {
+
         return currentStation;
     }
 
-    public void setCurrentStation(int CurrentStation){
-        if (CurrentStation<0){
+    public void setCurrentStation(int CurrentStation) {
+        if (CurrentStation < 0) {
             return;
         }
-        if (CurrentStation>9){
+        if (CurrentStation > 9) {
             return;
         }
-        this.currentStation=CurrentStation;
+        this.currentStation = CurrentStation;
     }
 
-    public void setSwitchToNextStation(int currentStation){
-        if (currentStation<9) {
-            this.currentStation=currentStation+1;
-    } else if (currentStation==9){
-            this.currentStation=0;
-        }
-    }
-    public void swithToPreviousStation(int currentStation){
-        if (currentStation==0){
-            this.currentStation=9;
-        }else if (currentStation<=9){
-            this.currentStation=currentStation-1;
-        }
-    }
-
-    public int getCurrentSoundLevel() {
-        return currentSoundLevel;
-    }
-    public void setCurrentSoundLevel(int currentSoundLevel){
-        if (currentSoundLevel<0){
-            return;
-        }
-        if (currentSoundLevel>10){
-            return;
-        }
-        this.currentSoundLevel=currentSoundLevel;
-    }
-    public void switchToNextSoundLevel(int currentSoundLevel){
-        if (currentSoundLevel < 10) {
-            this.currentSoundLevel = currentSoundLevel + 1;
-        } else if (currentSoundLevel == 10) {
+    public void switchToNextStation(int currentStation) {
+        if (currentStation < 9) {
+            this.currentStation = currentStation + 1;
+        } else if (currentStation == 9) {
             this.currentStation = 0;
         }
     }
-    public void switchToPreviousSoundLevel(int currentSoundLevel){
-        if (currentSoundLevel==0){
-            this.currentSoundLevel=10;
-        }else if (currentSoundLevel<=10){
-            this.currentSoundLevel=currentSoundLevel-1;
+
+    public void swithToPreviousStation(int currentStation) {
+        if (currentStation == 0) {
+            this.currentStation = 9;
+        } else if (currentStation <= 9) {
+            this.currentStation = currentStation - 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume(int currentVolume) {
+        if (currentVolume < 10) {
+            this.currentVolume = currentVolume + 1;
+        } else if (currentVolume == 10) {
+            this.currentStation = 0;
+        }
+    }
+
+    public void reduceVolume(int currentVolume) {
+        if (currentVolume == 0) {
+            this.currentVolume = 10;
+        } else if (currentVolume <= 10) {
+            this.currentVolume = currentVolume - 1;
         }
     }
 
